f53178a84acbd44d69f1be2d33fb3e175475d790
Aditya <adityacsahu02@gmail.com>
2023-04-15 14:57:07 +0530

    fixed jdk version and dependencies

---
+++ src/test/java/com/csr/CSR_MNG/CsrMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSR.CSR_MNG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
